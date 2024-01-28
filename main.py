11a5e06323475148a346ad04253c6dc8fc6a6628
“Katarzyna <katarzyna.zajac@edu.uekat.pl>
2024-01-28 14:55:44 +0100

    obrazy

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+import cv2
+
+
 app = Flask(__name__)
 api = Api(app)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('dworzec.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+
+        return {'count': len(boxes)}
+
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
+api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
